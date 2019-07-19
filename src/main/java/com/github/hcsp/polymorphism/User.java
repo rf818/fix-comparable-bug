c4025c4f58907e145763507c370afc133535791c
zzxx9426 <mrxiao9426@gmail.com>
2019-07-19 12:57:04 +0800

    compareTo (#13)
    compareTo (#13)

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -47,6 +47,9 @@ public class User implements Comparable<User> {
     /** 老板说让我按照用户名排序 */
     @Override
     public int compareTo(User o) {
+        if (this.name.equals(o.name)){
+            return -1;
+        }
         return name.compareTo(o.name);
     }
 
