1a26aaf98a3a939f665a1a910224ef47ca27012a
shiliyu <34638563+shiliyu@users.noreply.github.com>
2019-07-28 20:53:15 +0800

    fix comparable bug (#22)
    fix comparable bug (#22)

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -47,7 +47,13 @@ public class User implements Comparable<User> {
     /** 老板说让我按照用户名排序 */
     @Override
     public int compareTo(User o) {
-        return name.compareTo(o.name);
+        if (this.equals(o)) {
+            return 0;
+        }
+        if (this.name.equals(o.name)) {
+            return this.id.compareTo(o.id);
+        }
+        return this.name.compareTo(o.name);
     }
 
     public static void main(String[] args) {
