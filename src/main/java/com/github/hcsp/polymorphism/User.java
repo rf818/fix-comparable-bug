52bd682dc39fba89eb8de70be23b05ca94fc8f35
magicP <42081285+magicP@users.noreply.github.com>
2019-08-31 17:27:41 +0800

    修复compareTo方法的bug (#40)
    修复compareTo方法的bug (#40)

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -47,7 +47,11 @@ public class User implements Comparable<User> {
     /** 老板说让我按照用户名排序 */
     @Override
     public int compareTo(User o) {
-        return name.compareTo(o.name);
+         int result = name.compareTo(o.name);
+         if(result == 0){
+             result = id.compareTo(o.id);
+         }
+         return result;
     }
 
     public static void main(String[] args) {
