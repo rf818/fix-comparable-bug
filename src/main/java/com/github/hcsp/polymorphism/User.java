6ae70bb6659bbbaa84cca19c119bd4275f5cb089
keliangyong <337215966@qq.com>
2019-09-06 10:51:10 +0800

    treeSet使用compareTo比较两个元素 为0 说明两个元素相等 会被过滤 (#43)
    treeSet使用compareTo比较两个元素 为0 说明两个元素相等 会被过滤 (#43)

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -47,7 +47,11 @@ public class User implements Comparable<User> {
     /** 老板说让我按照用户名排序 */
     @Override
     public int compareTo(User o) {
-        return name.compareTo(o.name);
+        int compareResult = name.compareTo(o.name);
+        if (compareResult == 0) {
+            return id.compareTo(o.id);
+        }
+        return compareResult;
     }
 
     public static void main(String[] args) {
