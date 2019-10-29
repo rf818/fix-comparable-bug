5388840699976f5eb90f41ba7f3cbd6477c2e993
read&write <969552017@QQ.COM>
2019-10-29 19:28:45 +0800

    修复Comparable实现的bug (#69)
    修复Comparable实现的bug (#69)

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -47,6 +47,10 @@ public class User implements Comparable<User> {
     /** 老板说让我按照用户名排序 */
     @Override
     public int compareTo(User o) {
+        if(Objects.equals(name,o.name)
+            && !Objects.equals(id,o.id) ) {
+            return id.compareTo(o.id);
+        }
         return name.compareTo(o.name);
     }
 
