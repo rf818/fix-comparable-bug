cb5486d426e473c8073bcb87b79537769361e5b1
richard Hu <huyongjinnuaa@163.com>
2020-01-09 13:04:23 +0800

    comparable bug (#99)
    comparable bug (#99)

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -47,7 +47,10 @@ public class User implements Comparable<User> {
     /** 老板说让我按照用户名排序 */
     @Override
     public int compareTo(User o) {
-        return name.compareTo(o.name);
+        if(name.compareTo(o.name)==0){
+            return id.compareTo(o.id);
+        }
+        return name.compareTo(o.name) ;
     }
 
     public static void main(String[] args) {
