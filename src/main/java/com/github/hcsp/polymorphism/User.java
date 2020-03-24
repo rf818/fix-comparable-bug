ef08426eeb9306a0b1ba377181585b58827a00d3
gofocus <go_focus@live.cn>
2020-03-24 21:26:52 +0800

    修复Comparable实现中的bug (#138)
    修复Comparable实现中的bug (#138)

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -47,7 +47,12 @@ public class User implements Comparable<User> {
     /** 老板说让我按照用户名排序 */
     @Override
     public int compareTo(User o) {
-        return name.compareTo(o.name);
+        int i = name.compareTo(o.name);
+        if (i != 0) {
+            return i;
+        } else {
+            return id.compareTo(o.id);
+        }
     }
 
     public static void main(String[] args) {
