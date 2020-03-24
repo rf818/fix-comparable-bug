7f2e890682c87e5cdef582c152ba6e0f762486ad
xuepan-summer <54898607+xuepan-summer@users.noreply.github.com>
2020-03-24 21:22:12 +0800

    TreeSet踩坑 (#137)
    TreeSet踩坑 (#137)

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -6,10 +6,14 @@ import java.util.Objects;
 import java.util.TreeSet;
 
 public class User implements Comparable<User> {
-    /** 用户ID，数据库主键，全局唯一 */
+    /**
+     * 用户ID，数据库主键，全局唯一
+     */
     private final Integer id;
 
-    /** 用户名 */
+    /**
+     * 用户名
+     */
     private final String name;
 
     public User(Integer id, String name) {
@@ -44,9 +48,19 @@ public class User implements Comparable<User> {
         return id != null ? id.hashCode() : 0;
     }
 
-    /** 老板说让我按照用户名排序 */
+    /**
+     * 老板说让我按照用户名排序
+     */
     @Override
     public int compareTo(User o) {
+        if (name.compareTo(o.name) == 0) {
+            if (this.id != null && !(this.id.equals(o.id))) {
+                if (this.id > o.id) {
+                    return 1;
+                }
+                return -1;
+            }
+        }
         return name.compareTo(o.name);
     }
 
