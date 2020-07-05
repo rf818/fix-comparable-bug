31a2027ce111d36c9c1f5243927c9366f8279b41
陳雲濤 <36143626+0x522@users.noreply.github.com>
2020-07-05 14:42:26 +0800

    修复Comparable实现中的bug。 (#170)
    修复Comparable实现中的bug。 (#170)

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -6,10 +6,14 @@ import java.util.Objects;
 import java.util.TreeSet;
 
 public class User implements Comparable<User> {
-    /** 用户ID，数据库主键，全局唯一 */
+    /**
+     * 用户ID，数据库主键，全局唯一
+     */
     private final Integer id;
 
-    /** 用户名 */
+    /**
+     * 用户名
+     */
     private final String name;
 
     public User(Integer id, String name) {
@@ -44,10 +48,21 @@ public class User implements Comparable<User> {
         return id != null ? id.hashCode() : 0;
     }
 
-    /** 老板说让我按照用户名排序 */
+    /**
+     * 老板说让我按照用户名排序
+     */
     @Override
     public int compareTo(User o) {
-        return name.compareTo(o.name);
+        if (name.compareTo(o.name) == 0) {
+            if (this.id.equals(o.id)) {
+                return 0;
+            } else if (this.id > o.id) {
+                return 1;
+            }
+        } else {
+            return name.compareTo(o.name);
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
