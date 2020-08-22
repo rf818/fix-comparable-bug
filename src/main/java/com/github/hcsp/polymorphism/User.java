6ba9439055f1cedf47f3ff0ba1d1cbd285f8343c
杨汉杰 <36122057+freshmanyang@users.noreply.github.com>
2020-08-22 18:49:45 -0400

    fix comparable bug (#178)
    fix comparable bug (#178)

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -6,10 +6,14 @@ import java.util.Objects;
 import java.util.TreeSet;
 
 public class User implements Comparable<User> {
-    /** 用户ID，数据库主键，全局唯一 */
+    /**
+     * 用户ID，数据库主键，全局唯一
+     */
     private final Integer id;
 
-    /** 用户名 */
+    /**
+     * 用户名
+     */
     private final String name;
 
     public User(Integer id, String name) {
@@ -44,10 +48,14 @@ public class User implements Comparable<User> {
         return id != null ? id.hashCode() : 0;
     }
 
-    /** 老板说让我按照用户名排序 */
+    /**
+     * 老板说让我按照用户名排序
+     */
     @Override
     public int compareTo(User o) {
-        return name.compareTo(o.name);
+        int res = name.compareTo(o.name);
+        // if the name of two obj are equal, then compare their unique id.
+        return res != 0 ? res : id.compareTo(o.id);
     }
 
     public static void main(String[] args) {
