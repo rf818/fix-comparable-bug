f1798392c4fa1a849a5eb747e3aa05c09c15d729
qpWei <42760426+qipengwei@users.noreply.github.com>
2020-11-23 23:00:32 +0800

    修复bug (#189)
    修复bug (#189)

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -47,7 +47,13 @@ public class User implements Comparable<User> {
     /** 老板说让我按照用户名排序 */
     @Override
     public int compareTo(User o) {
-        return name.compareTo(o.name);
+        if (name.compareTo(o.name) > 0) {
+            return 1;
+        } else if (name.compareTo(o.name) < 0) {
+            return -1;
+        }
+
+        return id.compareTo(o.getId());
     }
 
     public static void main(String[] args) {
