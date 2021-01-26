42f68a373d088f686d7f03d72c4c2c172362a867
2014 <suhanma@gmail.com>
2021-01-25 17:15:26 -0800

    compare (#194)
    compare (#194)

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -47,7 +47,9 @@ public class User implements Comparable<User> {
     /** 老板说让我按照用户名排序 */
     @Override
     public int compareTo(User o) {
-        return name.compareTo(o.name);
+        int comp = name.compareTo(o.name);
+        if(comp==0) return id.compareTo(o.id);
+        return comp;
     }
 
     public static void main(String[] args) {
