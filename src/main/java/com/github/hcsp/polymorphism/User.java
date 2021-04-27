a594d20217a886952d42bcf253a09940cc55f459
giantpetter <65336996+giantpetter@users.noreply.github.com>
2021-04-27 15:30:51 +0800

    记住compareto的比较约定和set集合不包含重复元素 (#200)
    记住compareto的比较约定和set集合不包含重复元素 (#200)

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -6,10 +6,14 @@ import java.util.Objects;
 import java.util.TreeSet;
 
 public class User implements Comparable<User> {
-    /** 用户ID，数据库主键，全局唯一 */
+    /**
+     * 用户ID，数据库主键，全局唯一
+     */
     private final Integer id;
 
-    /** 用户名 */
+    /**
+     * 用户名
+     */
     private final String name;
 
     public User(Integer id, String name) {
@@ -44,9 +48,14 @@ public class User implements Comparable<User> {
         return id != null ? id.hashCode() : 0;
     }
 
-    /** 老板说让我按照用户名排序 */
+    /**
+     * 老板说让我按照用户名排序
+     */
     @Override
     public int compareTo(User o) {
+        if (name.equals(o.name)) {
+            return id - o.id;
+        }
         return name.compareTo(o.name);
     }
 
