633273d1428f81a620eadf950d3561ebc09a3e83
bowen-wu <33862944+bowen-wu@users.noreply.github.com>
2021-06-08 19:52:24 +0800

    fix: fixed compareTo method (#202)
    fix: fixed compareTo method (#202)

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -47,6 +47,9 @@ public class User implements Comparable<User> {
     /** 老板说让我按照用户名排序 */
     @Override
     public int compareTo(User o) {
+        if (name.compareTo(o.name) == 0) {
+            return id.compareTo(o.getId());
+        }
         return name.compareTo(o.name);
     }
 
