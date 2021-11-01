ad3c63e72697668487ac80bc2b25ce639d74dbfc
小虾米 <freebufer.wang@gmail.com>
2021-11-02 02:17:59 +0800

    Fix bug (#218)
    Fix bug (#218)

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -6,10 +6,14 @@ import java.util.Objects;
 import java.util.TreeSet;
 
 public class User implements Comparable<User> {
-    /** 用户ID，数据库主键，全局唯一 */
+    /**
+     * 用户ID，数据库主键，全局唯一
+     */
     private final Integer id;
 
-    /** 用户名 */
+    /**
+     * 用户名
+     */
     private final String name;
 
     public User(Integer id, String name) {
@@ -44,10 +48,12 @@ public class User implements Comparable<User> {
         return id != null ? id.hashCode() : 0;
     }
 
-    /** 老板说让我按照用户名排序 */
+    /**
+     * 老板说让我按照用户名排序
+     */
     @Override
     public int compareTo(User o) {
-        return name.compareTo(o.name);
+        return name.compareTo(o.name) == 0 ? id.compareTo(o.id) : name.compareTo(o.name);
     }
 
     public static void main(String[] args) {
