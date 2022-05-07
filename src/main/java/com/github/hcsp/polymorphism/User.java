cc441e66afab30cd7231ace75b4ebc8b2a3c0c07
Cao Ruiqi <rqcao@outlook.com>
2022-05-08 00:56:31 +0800

    feat: 更改代码 (#237)
    feat: 更改代码 (#237)

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -47,6 +47,14 @@ public class User implements Comparable<User> {
     /** 老板说让我按照用户名排序 */
     @Override
     public int compareTo(User o) {
+        if(name.equals(o.name)){
+            if(id < o.id){
+                return -1;
+            } else if (id > o.id) {
+                return 1;
+            }
+            return 0;
+        }
         return name.compareTo(o.name);
     }
 
