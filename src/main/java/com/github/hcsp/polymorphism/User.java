a7eff150566716c740181ab98f521cc894399153
lei <106215839+3stonelei@users.noreply.github.com>
2022-05-30 23:40:18 +0800

    修复bug (#238)
    修复bug (#238)

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -47,7 +47,10 @@ public class User implements Comparable<User> {
     /** 老板说让我按照用户名排序 */
     @Override
     public int compareTo(User o) {
-        return name.compareTo(o.name);
+        if (this.name.equals(o.name)) {
+            return this.id.compareTo(o.id);
+        }
+        return this.name.compareTo(o.name);
     }
 
     public static void main(String[] args) {
