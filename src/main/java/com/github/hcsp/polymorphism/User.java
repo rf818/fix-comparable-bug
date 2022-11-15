465c70610701f6414d3aec5fed25e12a86f552af
Zero <fan552426811@outlook.com>
2022-11-15 23:41:43 +0800

    compareTo (#246)
    compareTo (#246)

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -47,7 +47,12 @@ public class User implements Comparable<User> {
     /** 老板说让我按照用户名排序 */
     @Override
     public int compareTo(User o) {
-        return name.compareTo(o.name);
+        int res = name.compareTo(o.name);
+        if(res>0||res<0){
+            return res;
+        }else{
+            return id.compareTo(o.id);
+        }
     }
 
     public static void main(String[] args) {
